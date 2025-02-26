edf4612eedc8c65d9389226eb54b856154eb1e49
P-H-Pancholi <pancholiphalin@outlook.com>
2025-02-26 23:06:16 +0530

    Register command

--- Gator/main.go
+++ Gator/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
+
+	_ "github.com/lib/pq"
 
 	"github.com/P-H-Pancholi/Golang-Projects/gator/internal/config"
+	"github.com/P-H-Pancholi/Golang-Projects/gator/internal/database"
 )
 
 type state struct {
-	c *config.Config
+	db *database.Queries
+	c  *config.Config
 }
 
 type command struct {
@@ -39,10 +46,43 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("expected one argument, got zero")
 	}
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("no user with given username")
+	}
 	if err := s.c.SetUser(cmd.args[0]); err != nil {
 		return err
 	}
 	fmt.Println("The user has been set")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("expected one argument, got zero")
+	}
+
+	_, err := s.db.GetUser(context.Background(), cmd.args[0])
+	if err == nil {
+		return fmt.Errorf("user already exists")
+	}
+	if err != sql.ErrNoRows {
+		return err
+	}
+	arg := database.CreateUserParams{
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+	user, err := s.db.CreateUser(context.Background(), arg)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s user is created with id %d\n", user.Name, user.ID)
+	if err := s.c.SetUser(cmd.args[0]); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -51,13 +91,18 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	db, err := sql.Open("postgres", c.DbURL)
+	dbQueries := database.New(db)
 	s := state{
-		c: &c,
+		c:  &c,
+		db: dbQueries,
 	}
+
 	comm := commands{
 		make(map[string]func(*state, command) error),
 	}
 	comm.register("login", handlerLogin)
+	comm.register("register", handlerRegister)
 	if len(os.Args) < 3 {
 		fmt.Println("Expected more than 1 args")
 		os.Exit(1)
---
+++ Gator/sql/queries/users.sql
@@ -0,0 +1,14 @@
+-- name: CreateUser :one
+INSERT INTO users (created_at, updated_at, name)
+VALUES (
+    $1,
+    $2,
+    $3
+)
+RETURNING *;
+
+-- name: GetUser :one
+SELECT * FROM users WHERE name = $1;
+
+-- name: DeleteAllUser :exec
+DELETE FROM users
